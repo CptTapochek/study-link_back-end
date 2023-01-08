81c92449f32ec7e22fde00b46de211e3341ac762
CptTapo4ek <andreib@nextlogic.ro>
2023-01-08 19:26:17 +0200

    quiz graphql schema

--- gql/resolvers/index.js
+++ gql/resolvers/index.js
@@ -1,2 +1,3 @@
 export { getCoursesList, getCourse, createCourse } from "../../server/api/courses.js";
 export { login, SignUp } from "../../server/api/users.js";
+export { getQuiz } from "../../server/api/quiz.js";
